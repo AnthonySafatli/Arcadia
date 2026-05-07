b2ec90b2d89d372e51d9aa775ecdc48e85de6c8b
Anthony Safatli <anthonydaniel123@live.com>
2026-05-07 16:45:26 -0300

    Made useGameRoom a singleton

--- client/src/composables/useGameRoom.ts
+++ client/src/composables/useGameRoom.ts
@@ -1,65 +1,50 @@
-import { ref, onUnmounted } from "vue";
+import { ref } from "vue";
 import { toast } from "vue3-toastify";
 import { useSocket } from "@/composables/useSocket";
 import type { Room } from "@/dtos/RoomDto";
-import type { GameStartEvent, GameOverEvent, GameStateEvent } from "@/dtos/SocketEventDto";
+
+const { socket } = useSocket();
+const room = ref<Room | null>(null);
+const state = ref<unknown | null>(null);
+const connected = ref(false);
+
+socket.on("joined", (data) => {
+	connected.value = true;
+	room.value = data.room;
+});
+
+socket.on("player_joined", (data) => {
+	room.value = data.room;
+});
+socket.on("player_reconnected", (data) => {
+	room.value = data.room;
+});
+socket.on("player_disconnected", (data) => {
+	room.value = data.room;
+});
+
+socket.on("player_renamed", (data) => {
+	const player = room.value?.players.find((x) => x.player_id == data.player_id);
+	if (player) player.nickname = data.nickname;
+});
+
+socket.on("error", (data) => {
+	console.error(data.message);
+	toast(data.message, { theme: "dark", type: "error", position: "bottom-center" });
+});
+
+socket.on("game_start", (data) => {
+	room.value = data.room;
+	state.value = data.state;
+});
+
+socket.on("game_over", (data) => {
+	room.value = data.room;
+});
+socket.on("game_state", (data) => {
+	state.value = data.state;
+});
 
 export function useGameRoom() {
-	const { socket } = useSocket();
-	const room = ref<Room | null>(null);
-	const state = ref<unknown | null>(null);
-	const connected = ref(false);
-
-	socket.on("joined", (data) => {
-		connected.value = true;
-		room.value = data.room;
-	});
-
-	socket.on("player_joined", (data) => {
-		room.value = data.room;
-	});
-	socket.on("player_reconnected", (data) => {
-		room.value = data.room;
-	});
-	socket.on("player_disconnected", (data) => {
-		room.value = data.room;
-	});
-
-	socket.on("player_renamed", (data) => {
-		const player = room.value?.players.find((x) => x.player_id == data.player_id);
-		if (player) player.nickname = data.nickname;
-	});
-
-	socket.on("error", (data) => {
-		console.error(data.message);
-		toast(data.message, { theme: "dark", type: "error", position: "bottom-center" });
-	});
-
-	socket.on("game_start", (data) => {
-		room.value = data.room;
-		state.value = data.state;
-	});
-
-	socket.on("game_over", (data) => {
-		room.value = data.room;
-	});
-
-	socket.on("game_state", (data) => {
-		state.value = data.state;
-	});
-
-	onUnmounted(() => {
-		socket.off("joined");
-		socket.off("player_joined");
-		socket.off("player_reconnected");
-		socket.off("player_disconnected");
-		socket.off("player_renamed");
-		socket.off("error");
-		socket.off("game_start");
-		socket.off("game_over");
-		socket.off("game_state");
-		socket.disconnect();
-	});
-
 	return { room, state, connected };
 }
